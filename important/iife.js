c3e86238e942088510e39e1c5122955ee29891ee
Anmol Pandya <150453488+anmollpandyaa@users.noreply.github.com>
2026-03-25 15:23:52 +0000

    iife

---
+++ important/iife.js
@@ -0,0 +1,16 @@
+// iife is like a private room
+// used to avoid global scope pollution problem
+(function getName() {
+    name = "Anmoll"
+    console.log(name)
+})()
+
+// package function into an expression, like we did in arrow functions
+// then add function call () immediately after function ends
+// semicolon is required to end context after writing iife, sometimes may not produce error but recommended
+
+// treat it just like a function, below i wrote arrow function, passed arguments, wrapped inside console.log
+console.log(((num1, num2) => num1 + num2)(1602, 402))
+
+// iifes are good for temporary calculation and helper function
+// named iife means writing a function with name, unnamed can be arrow one
