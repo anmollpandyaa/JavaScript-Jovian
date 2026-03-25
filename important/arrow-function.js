61cc309f01cf7c404d5b78bbf5640ffbc87fa5eb
Anmol Pandya <150453488+anmollpandyaa@users.noreply.github.com>
2026-03-25 15:00:38 +0000

    arro function

---
+++ important/arrow-function.js
@@ -0,0 +1,20 @@
+// normal function
+function add(num1, num2) {
+    return num1 + num2
+}
+
+// arrow function
+const addNum = (num1, num2) => {
+    return num1 + num2
+}
+
+// implicit return way
+const addNums = (num1, num2) => num1 + num2
+
+// another way, often used in react
+const addNumsOnceAgain = (num1, num2) => ( num1, num2 )
+
+// return object
+const getName = () => ({name: "Anmoll", skill: "pretty much everything"})
+
+console.log(getName())
