b1ba2c2e67fe76ebe78386ed9fbb606fcfd27627
Anmol Pandya <150453488+anmollpandyaa@users.noreply.github.com>
2026-03-30 12:14:26 +0000

    for each loop uses

---
+++ important/for-each.js
@@ -0,0 +1,49 @@
+// this is a higher order loop, knows pretty much things already
+// has access to three properties: item, index and arr (name can vary, sequence is fixed)
+
+const furniture = ['table', 'bed', 'side table', 'shoe rack']
+
+// we pass a callback inside for each, so it has no name
+// as we are already performing loop on a specific array or object, we do not need to explicitly pass it
+furniture.forEach(function(item) {
+    console.log(item);
+    
+})
+
+console.log("------------------------");
+
+furniture.forEach((item, index) => {
+    console.log(`Index of ${item} is ${index}`);
+    
+})
+
+console.log("------------------------");
+
+// we can also directly pass a function inside for each
+function printName(i, j){
+    console.log(i, j)
+}
+
+furniture.forEach(printName)
+
+console.log("------------------------");
+
+// this is how we can loop through array of objects, and can access specific data as well
+const team = [
+{
+    name: 'Anmoll Pandya',
+    socials: '@anmollpandyaa',
+    age: 22,
+    interests: ['Geopolitics', 'Social Media']
+},
+{
+    name: 'Chaya Jargurjar',
+    socials: '@chayaa',
+    age: 21,
+    interests: ['Social Media', 'Literature']
+}
+]
+
+team.forEach((item) => {
+    console.log(item.socials);
+})
