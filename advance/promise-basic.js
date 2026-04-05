f7a7fb8e375fa4f25cc4375e5f1358f4a5fb5400
Anmol Pandya <150453488+anmollpandyaa@users.noreply.github.com>
2026-04-05 10:42:05 +0000

    consuming promise using async

--- advance/promise-basic.js
+++ advance/promise-basic.js
@@ -6,7 +6,7 @@ new Promise(function (resolve, reject) { // creating a promise
     console.log(c);
     resolve()
 }).then(function () {                    // consuming a promise
-    console.log("executed");
+    console.log("executed")
 })
 
 // we can also pass data from a promise
@@ -23,9 +23,29 @@ new Promise(function (resolve, reject) {
     if (!error) resolve({ success: "pass" })
     else reject({ success: "fail" })
 }).then(function (response) {
-    console.log(response);
+    console.log(response)
 }).catch(function (issue) {
     console.log(issue)
 }).finally(function () {
     console.log("Task Finished")
 })
+
+// we can also consume a promise using async await, or can just do .then .catch like above
+
+const newPromise = new Promise(function(resolve, reject){
+    let problem = true
+
+    if(!problem) resolve({status: "good"})
+    else reject({status: "bad"})
+})
+
+async function consumeNewPromise(){
+    try {
+        const response = await newPromise
+        console.log(response)
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+consumeNewPromise()
