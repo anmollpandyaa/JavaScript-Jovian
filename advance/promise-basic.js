164dfe8c1e4e9e1301c9a11c373f0ea3fb77f0c3
Anmol Pandya <150453488+anmollpandyaa@users.noreply.github.com>
2026-04-05 10:32:35 +0000

    creating a promise with resolve and reject

--- advance/promise-basic.js
+++ advance/promise-basic.js
@@ -1,17 +1,31 @@
-new Promise(function(resolve, reject){
+new Promise(function (resolve, reject) { // creating a promise
     let a = 10
     let b = 90
 
     let c = a + b
     console.log(c);
     resolve()
-}).then(function () {
+}).then(function () {                    // consuming a promise
     console.log("executed");
 })
 
 // we can also pass data from a promise
-new Promise(function(resolve, reject){
-    resolve({username: "@anmollpandyaa", designation: "CEO of Procrastination"})
-}).then(function(user){
+new Promise(function (resolve, reject) {
+    resolve({ username: "@anmollpandyaa", designation: "CEO of Procrastination" })
+}).then(function (user) {
     console.log(user);
+})
+
+// creating a promise with resolve and reject
+new Promise(function (resolve, reject) {
+    let error = false
+
+    if (!error) resolve({ success: "pass" })
+    else reject({ success: "fail" })
+}).then(function (response) {
+    console.log(response);
+}).catch(function (issue) {
+    console.log(issue)
+}).finally(function () {
+    console.log("Task Finished")
 })
